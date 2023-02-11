a961a36d715a3d765866be93fef9a00d4dbbbe4c
Fajar Alif Riyandi <fajariyandi36@gmail.com>
2023-02-12 00:02:28 +0700

    update: - fixed MainActivity Bug (2 instance ketika launch app) - Api token tidak akan digunakan untuk sementara waktu

--- app/src/main/java/com/lukman/githubuser/Splash.kt
+++ app/src/main/java/com/lukman/githubuser/Splash.kt
@@ -1,25 +1,15 @@
 package com.lukman.githubuser
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.ViewModelProvider
-import com.lukman.githubuser.Helper.SettingPreference
-import com.lukman.githubuser.Helper.ViewModelFactory
-import com.lukman.githubuser.ViewModel.SplashViewModel
+
 import com.lukman.githubuser.databinding.ActivitySplashBinding
 
 
 class Splash : AppCompatActivity() {
-    private val Context.dataStore:DataStore<Preferences> by preferencesDataStore(name = "Setting")
     private var _binding:ActivitySplashBinding?=null
     private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,30 +18,12 @@ class Splash : AppCompatActivity() {
         setContentView(binding?.root)
         supportActionBar?.hide()
         val handler = Handler(Looper.getMainLooper())
-        val pref = SettingPreference.getInstance(dataStore)
-        val splashViewModel:SplashViewModel = obtainViewModel(this)
-        splashViewModel.getThemeSetting(pref).observe(this){ isDarkModeActive ->
-            if(isDarkModeActive){
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                binding?.root?.background = AppCompatResources.getDrawable(this, R.drawable.bg_black)
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, tDelay)
 
-            }else{
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                binding?.root?.background = AppCompatResources.getDrawable(this, R.drawable.bg_grey)
-            }
-            handler.postDelayed({
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            }, tDelay)
-        }
-
-
-    }
-
-    private fun obtainViewModel(activity: AppCompatActivity): SplashViewModel {
-        val factory = ViewModelFactory.getInstance(activity.application)
-        return ViewModelProvider(activity, factory)[SplashViewModel::class.java]
     }
 
     override fun onDestroy() {
